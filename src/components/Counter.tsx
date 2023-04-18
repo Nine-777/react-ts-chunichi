0f05f4632a6f817d34d7a77450137451bc68dab7
Nine-777 <hkearth777@gmail.com>
2023-04-18 19:07:22 +0900

    フォーマット

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,25 +1,33 @@
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
+import { useState } from "react";
+import Button from "@mui/material/Button";
 
 type nameType = {
   userName: string;
 };
 
-export const Counter = (props: nameType) => {
+const Counter = (props: nameType) => {
   const [count, setCount] = useState(0);
 
   const incrementCount = () => {
     setCount(count + 1);
-  }
+  };
 
   return (
     <div>
-      <td>{ props.userName }</td>
-      <td>{ count }</td>
-      <Button variant="contained" color="primary" onClick={incrementCount}>+</Button>
-      <Button variant="contained" color="primary" onClick={() => count > 0 && setCount(count - 1)}>-</Button>
+      <td>{props.userName}</td>
+      <td>{count}</td>
+      <Button variant="contained" color="primary" onClick={incrementCount}>
+        +
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => count > 0 && setCount(count - 1)}
+      >
+        -
+      </Button>
     </div>
   );
-}
+};
 
 export default Counter;
